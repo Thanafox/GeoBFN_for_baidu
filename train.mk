7dede4861d27ecb7f93c286c9d77126b83642042
Thanafox <157661564+Thanafox@users.noreply.github.com>
2025-04-13 07:59:38 +0800

    更改训练脚本

--- train.mk
+++ train.mk
@@ -1,5 +1,5 @@
 SHELL := /bin/bash
 
 run:
-	[[ -d dataset/qm9 ]] || (set -e; ([[ -e qm9.tar.gz ]]|| ( gdown "https://drive.google.com/file/d/1erfnk2CaeqAGaMc9L95WI9ZEvZz5GHrl/view?usp=sharing" --fuzzy)); mkdir -p dataset; tar -xvf qm9.tar.gz -C dataset;)
+	[[ -d dataset/custom ]] || (set -e; ([[ -e cus.tar.gz ]]|| ( gdown "https://drive.google.com/file/d/1n5HCY6WuAy-i8A3IGd3G4VnoL_E8bYND/view?usp=sharing" --fuzzy)); mkdir -p dataset; tar -xvf qm9.tar.gz -C dataset;)
 	python geobfn_train.py --config_file configs/bfn4molgen.yaml --epochs 3000 --no_wandb
