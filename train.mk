98aec8e7974a370fb370d88ad3e9933d65c6c678
Thanafox <157661564+Thanafox@users.noreply.github.com>
2025-04-13 08:06:19 +0800

    Update train.mk

--- train.mk
+++ train.mk
@@ -1,5 +1,5 @@
 SHELL := /bin/bash
 
 run:
-	[[ -d dataset/custom ]] || (set -e; ([[ -e cus.tar.gz ]]|| ( gdown "https://drive.google.com/file/d/1n5HCY6WuAy-i8A3IGd3G4VnoL_E8bYND/view?usp=sharing" --fuzzy)); mkdir -p dataset; tar -xvf qm9.tar.gz -C dataset;)
+	[[ -d dataset/custom ]] || (set -e; ([[ -e custom.tar.gz ]]|| ( gdown "https://drive.google.com/file/d/1eQFBmZFIRACtKkhy8gw9d1Kh-aMr_Lb6/view?usp=sharing" --fuzzy)); mkdir -p dataset; tar -xvf qm9.tar.gz -C dataset;)
 	python geobfn_train.py --config_file configs/bfn4molgen.yaml --epochs 3000 --no_wandb
